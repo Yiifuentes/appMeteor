cfa28cc94a5b6761dc1b050ac8df4a4c80c76343
“Yiifuentes“ <fuentesl15@hotmail.com>
2018-01-30 21:04:28 -0500

    mejora en el helper

--- client/templates/home.js
+++ client/templates/home.js
@@ -10,6 +10,14 @@ Template.home.destroyed=function () {
 
 Template.home.helpers({
     ejemploHelper:function () {
-         return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
-    }
+        return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+    },
+    // other helpers ...
+    dataContextHelper: function(){
+        return {
+            someText: 'This text was set using a helper of the parent  template.',
+            someNested: {
+                text: 'That comes from "someNested.text"'
+            }
+        }; }
 });
